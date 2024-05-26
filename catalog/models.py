079e4fb585a0c6fe0204f36e56330cf6265aace0
Pobedilda <playfog3@gmail.com>
2024-05-26 21:55:52 +0300

    add Song, Performer, Genre  models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Performer(AbstractUser):
+    nickname = models.CharField(max_length=63, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "performer"
+        verbose_name_plural = "performers"
+
+    def __str__(self):
+        return f"{self.nickname} {self.first_name} {self.last_name}"
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    duration = models.IntegerField(max_length=255)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["tittle"]
+
+    def __str__(self):
+        return f"{self.title} {self.duration} {self.genre}"
