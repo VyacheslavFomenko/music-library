8c08d880e74b0595c99e7c8d552088b28c18d886
Pobedilda <playfog3@gmail.com>
2024-05-30 15:42:16 +0300

    add listener model, add AUTH_USER_MODEL, create template for sidebar and base html file

--- listener/models.py
+++ listener/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class Listener(AbstractUser):
+    class Meta:
+        ordering = ('username',)
+
+    def __str__(self):
+        return f"{self.username}, {self.first_name}, {self.last_name}."
--- music_library/settings.py
+++ music_library/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "catalog"
+    "catalog",
+    "listener"
 ]
 
 MIDDLEWARE = [
@@ -101,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-USER_MODEL = "catalog.Performer"
+AUTH_USER_MODEL = "listener.Listener"
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
