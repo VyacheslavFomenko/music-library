076283c035968f3307d19cd8136084ab15d44671
Pobedilda <playfog3@gmail.com>
2024-06-21 15:00:32 +0300

    add db models to admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from catalog.models import Song, Genre, Performer
+
 # Register your models here.
+
+admin.site.register(Song)
+admin.site.register(Genre)
+admin.site.register(Performer)
