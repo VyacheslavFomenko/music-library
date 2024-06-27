41aaeec2d1197cab3711cf7d73d9e081edc69a3e
Pobedilda <playfog3@gmail.com>
2024-06-27 19:15:51 +0300

    refactor: secure secrete key

--- music_library/settings.py
+++ music_library/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n@=vh!q@!r+t9e8t1uf(0yl-ar(wp(8y!vbdl&94om0-&wbls)"
+SECRET_KEY = os.getenv('SECRET_KEY')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 DEBUG_TOOLBAR_CONFIG = {"IS_RUNNING_TESTS": False}
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 INTERNAL_IPS = [
     "127.0.0.1",
